596c589923f4446da626b762907725a379f0d2bf
Jahanavi27 <Jahanavi27>
2026-05-27 11:05:39 +0530

    Initial website project

---
+++ script.js
@@ -0,0 +1,18 @@
+function showMessage() {
+    alert("Welcome to JavaScript!");
+}
+function changeHeading() {
+
+    document.getElementById("main-heading").innerText =
+        "I am becoming a Web Developer!";
+
+}
+function showName() {
+
+    let name =
+        document.getElementById("name-input").value;
+
+    document.getElementById("main-heading").innerText =
+        "Welcome " + name + "!";
+
+}
